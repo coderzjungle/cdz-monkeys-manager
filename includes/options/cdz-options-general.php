1f06ee3a696e91787d118ac57672c272df74f775
Conny Jacobs <mail@coderzjungle.com>
2015-08-21 14:44:02 +0200

    Changed: "Icons Options" position

--- includes/options/cdz-options-general.php
+++ includes/options/cdz-options-general.php
@@ -393,6 +393,133 @@ if ( ! function_exists( 'cdz_options_general' ) ) {
 
 		}
 
+		/*
+		 *	Icons Options
+		 *	Doc: https://github.com/audreyr/favicon-cheat-sheet
+		 */
+
+		if ( cdz_get_theme_options_std( 'info_icons' ) ) {
+
+			$options[] = array(
+				'name'			=>	__( 'Icons', 'cdz' ),
+				'desc'			=>	__( 'Upload the favicon and touch icons images', 'cdz' ),
+				'type'			=>	'info',
+			);
+
+			$options[] = array(
+				'name'			=>	__( 'Favicon', 'cdz' ),
+				'desc'			=>	__( '32x32 pixels', 'cdz' ),
+				'id'			=>	'opt_custom_icons_favicon',
+				'type'			=>	'upload',
+				'std'			=>	cdz_get_theme_options_std( 'opt_custom_icons_favicon' ),
+			);
+
+			/*
+			 *	Touch icon
+			 */
+
+			$options[] = array(
+				'name'			=>	__( 'Touch icons', 'cdz' ),
+				'desc'			=>	__( 'Enable advaced settings', 'cdz' ),
+				'id'			=>	'opt_custom_icons_touch',
+				'type'			=>	'checkbox',
+				'std'			=>	cdz_get_theme_options_std( 'opt_custom_icons_touch' ),
+			);
+
+			$options[] = array(
+				'name'			=>	__( ' ', 'cdz' ),
+				'desc'			=>	__( '152x152 pixels - for iPad with high-resolution Retina display running iOS >= 7', 'cdz' ),
+				'id'			=>	'opt_custom_icons_touch_152',
+				'class'			=>	'hidden',
+				'type'			=>	'upload',
+				'std'			=>	cdz_get_theme_options_std( 'opt_custom_icons_touch_152' ),
+			);
+
+			$options[] = array(
+				'name'			=>	__( ' ', 'cdz' ),
+				'desc'			=>	__( '144x144 pixels - for iPad with high-resolution Retina display running iOS <= 6', 'cdz' ),
+				'id'			=>	'opt_custom_icons_touch_144',
+				'class'			=>	'hidden',
+				'type'			=>	'upload',
+				'std'			=>	cdz_get_theme_options_std( 'opt_custom_icons_touch_144' ),
+			);
+
+			$options[] = array(
+				'name'			=>	__( ' ', 'cdz' ),
+				'desc'			=>	__( '120x120 pixels - for iPhone with high-resolution Retina display running iOS >= 7', 'cdz' ),
+				'id'			=>	'opt_custom_icons_touch_120',
+				'class'			=>	'hidden',
+				'type'			=>	'upload',
+				'std'			=>	cdz_get_theme_options_std( 'opt_custom_icons_touch_120' ),
+			);
+
+			$options[] = array(
+				'name'			=>	__( ' ', 'cdz' ),
+				'desc'			=>	__( '114x114 pixels - for iPhone with high-resolution Retina display running iOS <= 6', 'cdz' ),
+				'id'			=>	'opt_custom_icons_touch_114',
+				'class'			=>	'hidden',
+				'type'			=>	'upload',
+				'std'			=>	cdz_get_theme_options_std( 'opt_custom_icons_touch_114' ),
+			);
+
+			$options[] = array(
+				'name'			=>	__( ' ', 'cdz' ),
+				'desc'			=>	__( '76x76 pixels - for the iPad mini and the first- and second-generation iPad on iOS >= 7', 'cdz' ),
+				'id'			=>	'opt_custom_icons_touch_76',
+				'class'			=>	'hidden',
+				'type'			=>	'upload',
+				'std'			=>	cdz_get_theme_options_std( 'opt_custom_icons_touch_76' ),
+			);
+
+			$options[] = array(
+				'name'			=>	__( ' ', 'cdz' ),
+				'desc'			=>	__( '72x72 pixels - for the iPad mini and the first- and second-generation iPad on iOS <= 6', 'cdz' ),
+				'id'			=>	'opt_custom_icons_touch_72',
+				'class'			=>	'hidden',
+				'type'			=>	'upload',
+				'std'			=>	cdz_get_theme_options_std( 'opt_custom_icons_touch_72' ),
+			);
+
+			$options[] = array(
+				'name'			=>	__( ' ', 'cdz' ),
+				'desc'			=>	__( '57x57 pixels - for non-Retina iPhone, iPod Touch, and Android 2.1+ devices', 'cdz' ),
+				'id'			=>	'opt_custom_icons_touch_57',
+				'class'			=>	'hidden',
+				'type'			=>	'upload',
+				'std'			=>	cdz_get_theme_options_std( 'opt_custom_icons_touch_57' ),
+			);
+
+			/*
+			 *	IE 10 Metro tile icon
+			 */
+
+			$options[] = array(
+				'name'			=>	__( 'IE 10 Metro', 'cdz' ),
+				'desc'			=>	__( 'Enable IE 10 Metro tile icon', 'cdz' ),
+				'id'			=>	'opt_custom_icons_ie_10',
+				'type'			=>	'checkbox',
+				'std'			=>	cdz_get_theme_options_std( 'opt_custom_icons_ie_10' ),
+			);
+
+			$options[] = array(
+				'name'			=>	__( 'Tile Image', 'cdz' ),
+				'desc'			=>	__( '144x144 pixels', 'cdz' ),
+				'id'			=>	'opt_custom_icons_ie_10_metro_tile_image',
+				'class'			=>	'hidden',
+				'type'			=>	'upload',
+				'std'			=>	cdz_get_theme_options_std( 'opt_custom_icons_ie_10_metro_tile_image' ),
+			);
+
+			$options[] = array(
+				'name'			=>	__( 'Tile Color', 'cdz' ),
+				'id'			=>	'opt_custom_icons_ie_10_metro_tile_color',
+				'class'			=>	'hidden',
+				'type'			=>	'color',
+				'std'			=>	cdz_get_theme_options_std( 'opt_custom_icons_ie_10_metro_tile_color' ),
+			);
+
+		}
+
 		return $options;
 
 	}
